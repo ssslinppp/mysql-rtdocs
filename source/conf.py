97fe4e41733a87e759ef59f2de965696eb934af7
ssslinppp <liulin_sssppp@163.com>
2018-08-26 12:55:11 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -114,11 +114,11 @@ html_theme_options = {
     'analytics_id': '',
     'logo_only': False,
     'display_version': True,
-    'prev_next_buttons_location': 'bottom',
+    'prev_next_buttons_location': 'both',
     'style_external_links': False,
     'vcs_pageview_mode': '',
     # Toc options
-    'collapse_navigation': True,
+    'collapse_navigation': False,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
